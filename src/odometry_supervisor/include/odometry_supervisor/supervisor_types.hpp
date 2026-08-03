8cac3ce2fe988051b256f32e090b1ac7f1916fc4
TanDatEmb <TanDat.Emb@gmail.com>
2026-08-03 12:08:37 +0700

    fix(supervisor): expose aligned comparison age

--- src/odometry_supervisor/include/odometry_supervisor/supervisor_types.hpp
+++ src/odometry_supervisor/include/odometry_supervisor/supervisor_types.hpp
@@ -171,6 +171,7 @@ struct SupervisorOutput {
   std::int64_t lio_corrected_age_ns{-1};
   std::int64_t px4_age_ns{-1};
   std::int64_t alignment_gap_ns{-1};
+  std::int64_t aligned_comparison_age_ns{-1};
   Residual residual;
   std::uint64_t px4_reset_generation{0};
   std::uint64_t px4_time_generation{0};
